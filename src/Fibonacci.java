5473341f0c33b5a9e601dde36a71325dc7f778a3
chrisdalbano12 <chrisdalbano12@gmail.com>
2025-05-09 18:41:38 -0400

    Adding Fibonacci and Main class to main branch

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,35 @@
 public class Fibonacci {
+    static void recursiveSequence(int n) {
+        int n1 = 0, n2 = 1;
+
+        for (int i =0; i < n; i++){
+            //print number
+           System.out.print(n1 + " ");
+
+            // Swapping numbers
+            int n3 = n2 + n1;
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
+    static void getNthTerm(int n, int nthTerm) {
+        if (nthTerm > n) {
+            throw new IllegalArgumentException("Parameter nthTerm must be less than n");
+        }
+
+        int n1 = 0, n2 = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i == nthTerm - 1) {
+                System.out.printf("The %d-th term of the Fibonacci sequence is %d\n", nthTerm, n1);
+                return;
+            }
+
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
 }
